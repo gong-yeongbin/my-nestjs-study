f71419950309d93b744c0e06e7caec6aefb70e71
yeongbin-gong <youngbin816@gmail.com>
2023-11-10 21:31:55 +0900

    feat(config): config 기능 추가
    config 기능 추가, .env file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(3000);
+	const configService = app.get(ConfigService);
+	const port: number = configService.get<number>('PORT', 3000);
+
+	await app.listen(port);
 }
 bootstrap();
