070edc052573f91954be7f0eafe5adac7be82620
yeongbin-gong <youngbin816@gmail.com>
2023-11-17 01:13:17 +0900

    feat(swagger): swagger set
    swagger set

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -14,6 +15,11 @@ async function bootstrap() {
 			},
 		})
 	);
+
+	const config = new DocumentBuilder().setTitle('에코앤리치 과제 api').setVersion('1.0').addTag('echonrich').build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
+
 	const configService = app.get(ConfigService);
 	const port: number = configService.get<number>('PORT', 3000);
 
