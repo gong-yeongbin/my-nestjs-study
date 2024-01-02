cded7db25742f31ecd47531a4a5d938249a84538
gong-yeongbin <youngbin816@gmail.com>
2024-01-02 15:48:05 +0900

    feat(prisma): add prisma
    add prisma, create user table

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma.service';
 
 @Module({
 	imports: [ConfigModule.forRoot({ isGlobal: true, cache: true })],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [AppService, PrismaService],
 })
 export class AppModule {}
