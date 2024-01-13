422b5b5233b57bd1d746785311af89d7a1191217
gong-yeongbin <youngbin816@gmail.com>
2024-01-13 13:54:42 +0900

    feat(user): add user controller test
    add user controller test

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -15,10 +14,6 @@ async function bootstrap() {
 			},
 		})
 	);
-
-	const config = new DocumentBuilder().setTitle('에코앤리치 과제 api').setVersion('1.0').build();
-	const document = SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup('api', app, document);
 
 	const configService = app.get(ConfigService);
 	const port: number = configService.get<number>('PORT', 3000);
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dtos';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+	constructor(private userService: UserService) {}
+
+	@Post()
+	async create(@Body() createUserDto: CreateUserDto) {
+		const createUser = await this.userService.createUser(createUserDto);
+		return createUser;
+	}
+}
