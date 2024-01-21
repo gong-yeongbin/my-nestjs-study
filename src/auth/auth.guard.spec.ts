a331a8e647fc1628de7a8a41c6748293de272849
gong-yeongbin <youngbin816@gmail.com>
2024-01-21 22:22:40 +0900

    feat(auth): add auth guard extractTokenFromHeader unit test
    add auth guard extractTokenFromHeader unit test

--- src/auth/auth.guard.spec.ts
+++ src/auth/auth.guard.spec.ts
@@ -1,3 +1,42 @@
 import { AuthGuard } from './auth.guard';
+import { Request } from 'express';
+import { ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { HttpArgumentsHost } from '@nestjs/common/interfaces';
 
-describe('AuthGuard', () => {});
+describe('AuthGuard unit test', () => {
+	const authGuard = new AuthGuard();
+
+	it('extractTokenFromHeader unit test return undefied', () => {
+		const mockRequest = { headers: { authorization: '' } } as Request;
+
+		authGuard.extractTokenFromHeader = jest.fn().mockReturnValue(undefined);
+
+		expect(authGuard.extractTokenFromHeader(mockRequest)).toEqual(undefined);
+	});
+
+	it('extractTokenFromHeader unit test not Bearer return undefied', () => {
+		const mockRequest = { headers: { authorization: 'testtoke' } } as Request;
+
+		authGuard.extractTokenFromHeader = jest.fn().mockReturnValue(undefined);
+
+		expect(authGuard.extractTokenFromHeader(mockRequest)).toEqual(undefined);
+	});
+
+	it('extractTokenFromHeader unit test not Bearer return token', () => {
+		const mockRequest = { headers: { authorization: 'Bearer testtoken' } } as Request;
+
+		authGuard.extractTokenFromHeader = jest.fn().mockReturnValue('testtoken');
+		expect(authGuard.extractTokenFromHeader(mockRequest)).toEqual('testtoken');
+	});
+
+	// const mockContext = {
+	// 	switchToHttp: { getRequest: jest.fn() },
+	// } as any;
+	//
+	// it('auth guard canActivate token undefined UnauthorizedException test', () => {
+	// 	mockContext.switchToHttp.getRequest = jest.fn().mockReturnValue({});
+	// 	authGuard.extractTokenFromHeader = jest.fn().mockReturnValue(undefined);
+	//
+	// 	expect(() => authGuard.canActivate(mockContext)).toThrow(new UnauthorizedException());
+	// });
+});
--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,13 +1,19 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-	canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+	async canActivate(context: ExecutionContext): Promise<boolean> {
+		const request = context.switchToHttp().getRequest();
+		const token = this.extractTokenFromHeader(request);
+
+		if (!token) throw new UnauthorizedException();
+
 		return true;
 	}
 
-	private extractTokenFromHeader(request: Request): string | undefined {
-		return;
+	extractTokenFromHeader(request: Request): string | undefined {
+		const [type, token] = request.headers.authorization.split(' ') ?? [];
+		return type === 'Bearer' ? token : undefined;
 	}
 }
