36c297ef3f5151eecbfc2fe0ce56758f40b828ef
gong-yeongbin <youngbin816@gmail.com>
2024-02-27 07:05:10 +0900

    feat(app): add app controller unit test
    add app controller unit test

--- src/module/app/test/app.controller.spec.ts
+++ src/module/app/test/app.controller.spec.ts
@@ -9,6 +9,7 @@ import { UserModule } from '../../user/user.module';
 
 describe('AppController', () => {
 	let appController: AppController;
+	let appService: AppService;
 	let authService: AuthService;
 
 	beforeEach(async () => {
@@ -19,17 +20,51 @@ describe('AppController', () => {
 		}).compile();
 
 		appController = app.get<AppController>(AppController);
+		appService = app.get<AppService>(AppService);
 		authService = app.get<AuthService>(AuthService);
 	});
 
+	it('getHello, appService.getHello 호출 테스트 ', () => {
+		jest.spyOn(appService, 'getHello');
+
+		appController.getHello();
+		expect(appService.getHello).toBeCalledTimes(1);
+	});
+
+	it('getHello, appService.getHello 리턴 데이터 테스트', () => {
+		appService.getHello = jest.fn().mockReturnValue('hello world!!!');
+
+		expect(appController.getHello()).toEqual('hello world!!!');
+	});
+
 	const httpMock = {} as Request;
+	httpMock['user'] = { userId: 'mockUserId', username: 'mockUserName' };
+
+	it('login, authService.login 호출 테스트', () => {
+		jest.spyOn(authService, 'login');
+
+		appController.login(httpMock);
+		expect(authService.login).toBeCalledTimes(1);
+	});
 
 	it('login, get token', () => {
 		authService.login = jest.fn().mockReturnValue({ access_token: 'mock access token', refresh_token: 'mock refresh token' });
-		httpMock['userId'] = 'mockUserId';
-		httpMock['username'] = 'mockUserName';
 
 		const result = appController.login(httpMock);
 		expect(result).toEqual({ access_token: 'mock access token', refresh_token: 'mock refresh token' });
+	});
+
+	it('getAccessToken, getAccessToken 호출 테스트', () => {
+		jest.spyOn(authService, 'getAccessToken');
+
+		appController.getAccessToken(httpMock);
+		expect(authService.getAccessToken).toBeCalledTimes(1);
+	});
+
+	it('getAccessToken, getAccessToken', () => {
+		authService.getAccessToken = jest.fn().mockReturnValue({ access_token: 'mock access token' });
+
+		const result = appController.getAccessToken(httpMock);
+		expect(result).toEqual({ access_token: 'mock access token' });
 	});
 });
